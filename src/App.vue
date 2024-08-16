868d141543e26acd18041d42ce234cc02915ca19
Aleks D.V. <alex.norw.jap@gmail.com>
2024-08-17 02:40:18 +0300

    Third lesson

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
@@ -18,6 +19,22 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
+  <main class="flex flex-grow flex-col">
+    similique excepturi? Delectus, ut vitae cupiditate placeat nam inventore minima corporis.
+  </main>
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="justife-around flex items-center border-t">
+      <li class="flex flex-1 flex-col items-center p-2 text-xs capitalize">
+        <a href="#timeline"><ClockIcon h-6 w-6 />timeline</a>
+      </li>
+      <li class="flex flex-1 flex-col items-center p-2 text-xs capitalize">
+        <a href="#activities"><ListBulletIcon h-6 w-6 />activities</a>
+      </li>
+      <li class="flex flex-1 flex-col items-center p-2 text-xs capitalize">
+        <a href="#progress"><ChartBarIcon h-6 w-6 />progress</a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped></style>
