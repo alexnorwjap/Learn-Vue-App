f1f9e4102dab5c267d912aaa1edf5b5169fbac9e
Aleks D.V. <alex.norw.jap@gmail.com>
2024-08-18 01:23:10 +0300

    Lesson 8

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import NavItem from './NavItem.vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
@@ -7,12 +8,20 @@ const navItems = {
   actibities: ListBulletIcon,
   progress: ChartBarIcon
 }
+
+const currentPage = ref('')
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="justife-around flex items-center border-t">
-      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+        v-for="(icon, page) in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'pointer-events-none bg-gray-200': currentPage === page }"
+        @click="currentPage = page"
+      >
         <component :is="icon" class="h-6 w-6" />{{ page }}
       </NavItem>
     </ul>
