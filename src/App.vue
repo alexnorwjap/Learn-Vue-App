bf5ebda044c2fa2289ac118386278ab8bd9cde97
Aleks D.V. <alex.norw.jap@gmail.com>
2024-08-17 20:37:17 +0300

    Lesson 4,5,6

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,14 @@
 <script setup>
-import { CheckCircleIcon } from '@heroicons/vue/24/solid'
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import TheHeader from './components/TheHeader.vue'
+import TheNavigation from './components/TheNavigation.vue'
 </script>
 
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <a href="#">
-      <img src="./assets/img/logo.png" alt="logo" class="h-9" />
-    </a>
-    <a href="#" class="text-sm">
-      <div v-if="false" class="flex items-center gap-1">
-        Day complete!
-        <CheckCircleIcon class="h-7 text-green-500" />
-      </div>
-      <div v-else class="flex items-center gap-1">
-        <div>Progress: <span class="font-mono">20%</span></div>
-        <div class="bg-g h-3 w-3 rounded-full bg-red-500"></div>
-      </div>
-    </a>
-  </header>
+  <TheHeader />
   <main class="flex flex-grow flex-col">
     similique excepturi? Delectus, ut vitae cupiditate placeat nam inventore minima corporis.
   </main>
-  <nav class="sticky bottom-0 z-10 bg-white">
-    <ul class="justife-around flex items-center border-t">
-      <li class="flex flex-1 flex-col items-center p-2 text-xs capitalize">
-        <a href="#timeline"><ClockIcon h-6 w-6 />timeline</a>
-      </li>
-      <li class="flex flex-1 flex-col items-center p-2 text-xs capitalize">
-        <a href="#activities"><ListBulletIcon h-6 w-6 />activities</a>
-      </li>
-      <li class="flex flex-1 flex-col items-center p-2 text-xs capitalize">
-        <a href="#progress"><ChartBarIcon h-6 w-6 />progress</a>
-      </li>
-    </ul>
-  </nav>
+  <TheNavigation />
 </template>
 
 <style scoped></style>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = {
+  timeline: ClockIcon,
+  actibities: ListBulletIcon,
+  progress: ChartBarIcon
+}
+</script>
+
+<template>
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="justife-around flex items-center border-t">
+      <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
+        <a :href="`#${page}`" class="flex flex-1 flex-col items-center p-2 text-xs capitalize">
+          <component :is="icon" class="h-6 w-6" />{{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
